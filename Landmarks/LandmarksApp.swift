4b3a91bf4258cfb1852bb2a175d4ae83590d4e3c
Natalia Braz <naabraz@gmail.com>
2024-04-24 20:25:22 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Natalia Braz on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
